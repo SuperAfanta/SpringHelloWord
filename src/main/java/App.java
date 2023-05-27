2100405df9bfd742a7013b4cab87792d516d025e
SuperAfanta <thefallenknight@mail.ru>
2023-05-27 17:21:19 +0300

    test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld beanHelloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHelloWorld.getMessage());
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(beanHelloWorld == beanHelloWorld);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
